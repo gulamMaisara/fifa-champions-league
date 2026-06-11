8ee30a1833e4cea622ea4d7659ad33628c98ec2a
choudhg <choudhg@wwu.edu>
2026-06-11 02:47:33 -0700

    Updating json

--- vite.config.ts
+++ vite.config.ts
@@ -18,18 +18,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/",
-    mode: "production",
-  },
-  
   tanstackStart: {
     server: {
       entry: "server",
     },
   },
   
-
   nitro: {
     preset: "vercel",
   },
