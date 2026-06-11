be8b876404c06c0473f76b5ada925869cb134f1b
choudhg <choudhg@wwu.edu>
2026-06-11 01:47:37 -0700

    Fixing vercel

--- .vercel/output/functions/__server.func/_libs/tanstack__start-server-core.mjs
+++
@@ -1,9 +0,0 @@
-import { n as __exportAll } from "../_runtime.mjs";
-//#region node_modules/@tanstack/start-server-core/dist/esm/empty-plugin-adapters.js
-var empty_plugin_adapters_exports = /* @__PURE__ */ __exportAll({
-	hasPluginAdapters: () => false,
-	pluginSerializationAdapters: () => pluginSerializationAdapters
-});
-var pluginSerializationAdapters = [];
-//#endregion
-export { empty_plugin_adapters_exports as t };
--- .vercel/output/functions/__server.func/_ssr/matches._id-577k_5oM.mjs
+++
@@ -1,9 +0,0 @@
-import { f as lazyRouteComponent, p as createFileRoute } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/matches._id-577k_5oM.js
-var $$splitComponentImporter = () => import("./matches._id-Bhex0_6d.mjs");
-var Route = createFileRoute("/matches/$id")({
-	head: () => ({ meta: [{ title: "Match — FIFA Fantasy" }] }),
-	component: lazyRouteComponent($$splitComponentImporter, "component")
-});
-//#endregion
-export { Route as t };
